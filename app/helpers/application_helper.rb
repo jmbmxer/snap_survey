81aa80185e35b50967d6935b22dd2a0c4629cf39
Jimmy Mesta <jmesta@invoca.com>
2016-04-26 17:27:40 -0700

    search goodness and highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,22 @@
 module ApplicationHelper
-class CodeRayify < Redcarpet::Render::HTML
-  def block_code(code, language)
-    CodeRay.scan(code, language).div
-  end
-end
+  def markdown(text)
+    options = {
+      filter_html:     true,
+      hard_wrap:       true, 
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true, 
+      fenced_code_blocks: true
+    }
 
-def markdown(string)
-  @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true, fenced_code_blocks: true)
-  @markdown.render(string.to_s)
-end
+    extensions = {
+      autolink:           true,
+      superscript:        true,
+      disable_indented_code_blocks: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
 end
